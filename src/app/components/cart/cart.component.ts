7951c6f193018842c12c5caa0c950dd9134c05d4
gabriel <15620759+genrique98@users.noreply.github.com>
2021-07-30 10:12:25 -0700

    feat: added cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/Cart';
+import { CartService } from 'src/app/services/cart/cart.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrderService } from 'src/app/services/order/order.service';
+import { Order } from 'src/app/models/Order';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  total: number = 0;
+  cart: Cart[] = [];
+
+  name: string = ''
+  address: string = ''
+  card: number = 0
+
+  constructor(private cartService: CartService, private route: Router, private orderService: OrderService) { }
 
   ngOnInit(): void {
+    this.total = this.cartService.getTotalCost();
+    this.cart = this.cartService.getProductsInCart();
   }
 
+  onSubmit(): void {
+    let order: Order = {
+      name: this.name,
+      address: this.address,
+      card: this.card
+    }
+    alert(order.name)
+    this.orderService.updateOrder(order)
+
+    this.route.navigate(['/confirmation'])
+  }
 }
