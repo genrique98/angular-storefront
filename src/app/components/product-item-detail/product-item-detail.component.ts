f26ef7e36edea90ea22c06bd9d4fb2ba1bfa980a
gabriel <15620759+genrique98@users.noreply.github.com>
2021-07-30 10:16:27 -0700

    feat: added function to add to cart

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Product } from 'src/app/models/Product';
 import { ProductItemComponent } from '../product-item/product-item.component';
-
+import { ProductService } from 'src/app/services/product/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart/cart.service';
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
   styleUrls: ['./product-item-detail.component.css']
 })
-export class ProductItemDetailComponent extends ProductItemComponent {
+export class ProductItemDetailComponent implements OnInit {
 
   // @Input() product: Product;
   // @Output() addToCart: EventEmitter<Product> = new EventEmitter;
 
-  constructor() {
-    super();
-    this.currentProduct;
-   }
-  // constructor() { }
+  product: Product = new Product();
+  options: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute) {
+  }
+  
   ngOnInit(): void {
-    // alert(this.currentProduct.id);
+    this.route.url.subscribe(url => {
+      let id: number = (url.splice(1) as unknown) as number - 1
+      this.productService.getProducts().subscribe(res => {
+        this.product = res[id]
+      })
+    })
   }
 
+  addToCart(product: Product, quantity: number){
+    this.cartService.addProductToCart(product, quantity);
+    
+  }
 }
